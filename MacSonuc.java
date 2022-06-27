2d027901def2903f9a3936ad8ed7f8a62db80bc4
betulaltaas <79142159+betulaltaas@users.noreply.github.com>
2022-06-27 17:58:12 +0300

    Odev1

---
+++ MacSonuc.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+
+public class MacSonuc implements Serializable {
+
+    private String macSonuc;
+
+    public MacSonuc(String macSonuc) {
+        this.macSonuc = macSonuc;
+    }
+
+    public String getMacSonuc() {
+        return macSonuc;
+    }
+
+    public void setMacSonuc(String macSonuc) {
+        this.macSonuc = macSonuc;
+    }
+}
